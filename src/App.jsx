6cecbde4cfaac20482cfcd3031f254ced30da957
Onur Senel <onur.senel@monitise.com>
2017-04-09 21:18:07 +0300

    Add Redux, redux-promise and redux-form

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  BrowserRouter,
-  Route
-} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import rootReducer from 'common/rootReducer';
+import promiseMiddleware from 'redux-promise';
+import { Provider } from 'react-redux';
 import Feedback from 'modules/feedback/Feedback';
 
 const rootElement = document.getElementById('root');
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(promiseMiddleware)
+));
 
 ReactDOM.render(
-  <BrowserRouter>
-    <div className="app-container">
-      <Route exact path="/" component={Feedback} />
-    </div>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <div className="app-container">
+        <Route exact path="/" component={Feedback} />
+      </div>
+    </BrowserRouter>
+  </Provider>,
   rootElement
 );
